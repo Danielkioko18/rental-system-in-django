930672750010b4b6c7843fd9b903084eabc0f8ea
Daniel <danielkioko1844@gmail.com>
2025-01-06 13:58:46 +0300

    Houses frontend

--- rentals/urls.py
+++ rentals/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import DashboardView
+from .views import DashboardView,HousesView
 
 app_name = 'rentals'
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
-    path('houses/', views.houses, name='houses'),
+    path('houses/', HousesView.as_view(), name='houses'),
     #path('tenants/', views.tenants, name='tenants'),
     #path('reports/', views.reports, name='reports'),
 ]
