f72043293bdeb7a214ceaddae85b4553ee6c2a92
Daniel Kioko <danielkioko1844@gmail.com>
2026-04-15 12:28:44 +0300

    Debugged the logout functionallity

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import CustomLoginView
 from django.contrib.auth.views import LogoutView
 
+app_name = 'auth'
+
 urlpatterns = [
     path('', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
