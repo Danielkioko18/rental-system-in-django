500598764c89cf87ba22b632b11d308b1e6e7172
Daniel Kioko <danielkioko1844@gmail.com>
2026-04-18 16:39:47 +0300

    Login Update

--- authentication/forms.py
+++ authentication/forms.py
@@ -2,17 +2,25 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 class EmailLoginForm(AuthenticationForm):
+    error_messages = {
+        'invalid_login': 'Invalid email or password. Please try again.',
+        'inactive': 'Your account is inactive. Contact the administrator if you need access.',
+    }
+
     username = forms.EmailField(
         widget=forms.EmailInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Email'
+            'placeholder': 'Email',
+            'autocomplete': 'email',
+            'autofocus': 'autofocus',
         }),
         label="Email"
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Password'
+            'placeholder': 'Password',
+            'autocomplete': 'current-password',
         }),
         label="Password"
     )
